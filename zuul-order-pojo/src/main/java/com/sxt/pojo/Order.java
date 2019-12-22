4d31f27fd560656afc150dd5e346eacba6e10bf1
git <ssnwja@163.com>
2019-12-22 10:35:42 +0800

    STS使用git

---
+++ zuul-order-pojo/src/main/java/com/sxt/pojo/Order.java
@@ -0,0 +1,55 @@
+package com.sxt.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="t_order")
+public class Order implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	@Column
+	private String name;
+	@Column
+	private Integer money;
+	@Column
+	private String remark;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getMoney() {
+		return money;
+	}
+	public void setMoney(Integer money) {
+		this.money = money;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", name=" + name + ", money=" + money + ", remark=" + remark + "]";
+	}
+	
+	
+	
+}
